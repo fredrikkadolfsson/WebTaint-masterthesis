34ab6165d2f0bc8188998ac5d705f590a34aefc1
fredrik adolfsson <fredrikkadolfsson@gmail.com>
2018-04-19 18:58:30 +0200

    * Large rework and started adding sinks

---
+++ code/testPrograms/reflective_command_line_app/src/se/adolfsson/Main.java
@@ -0,0 +1,20 @@
+package se.adolfsson;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Reflective CommandLine App Starting...");
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter reflective content: ");
+        String reflect = scanner.next();
+
+        System.out.println(reflect);
+
+        System.out.println();
+        System.out.println("Reflective CommandLine App Ending");
+    }
+}
